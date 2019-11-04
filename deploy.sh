3928861e009976d4ce879a76d6bc035c61e19276
eksant <eksant@gmail.com>
2019-11-04 23:20:37 +0800

    Temp helper deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull origin master
+rm -rf node_modules/ yarn.lock && yarn
+if [ -d "./build" ]; then
+  rm -rf build
+fi
+yarn build
+rm -rf /var/www/medical.seorangeksa.com/*
+cp -R build/* /var/www/medical.seorangeksa.com
+
