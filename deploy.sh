4f66d28d357abe38b70725bc82e26f3eaa8d5b0f
eksant <eksant@gmail.com>
2019-11-06 01:36:27 +0800

    Simple deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 git pull origin develop
-rm -rf node_modules/ yarn.lock && yarn
+# rm -rf node_modules/ yarn.lock && yarn
 if [ -d "./build" ]; then
   rm -rf build
 fi
-yarn && yarn build
+# yarn 
+yarn build
 rm -rf /var/www/medical.seorangeksa.com/*
 cp -R build/* /var/www/medical.seorangeksa.com
 
