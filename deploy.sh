21e4f3f2d21f45144ea1ae4b8dfbf9126eb9993f
eksant <eksant@gmail.com>
2019-11-25 19:02:58 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-git pull origin develop
-# rm -rf node_modules/ yarn.lock && yarn
-if [ -d "./build" ]; then
-  rm -rf build
-fi
-# yarn 
-yarn build
-rm -rf /var/www/medical.seorangeksa.com/*
-cp -R build/* /var/www/medical.seorangeksa.com
+AWS_S3_REGION="ap-southeast-1b"
+STAGING_BRANCH="master"
+PRODUCTION_BRANCH="production"
 
+NODE_ENV="staging"
+CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_STAGING
+# if [[ $TRAVIS_BRANCH == $STAGING_BRANCH ]]; then
+#   NODE_ENV="staging"
+#   CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_STAGING
+#   yarn build
+# elif [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
+#   NODE_ENV="production"
+#   CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_PRODUCTION
+#   yarn build
+# else
+#   echo "Not deploying"
+#   exit
+# fi
+
+S3_BUCKET="$NODE_ENV.developer.pfalfa.io"
+echo "Deploying to the $S3_BUCKET bucket"
+
+pip install awscli --upgrade --user
+aws s3 sync public/ "s3://$S3_BUCKET" --acl public-read --delete
+
+aws cloudfront create-invalidation \
+  --distribution-id $CLOUDFRONT_DIST_ID \
+  --paths /*
+
+# git pull origin develop
+# rm -rf node_modules/ yarn.lock && yarn
+# if [ -d "./build" ]; then
+# rm -rf build
+# fi
+# yarn
+# yarn build
+# rm -rf /var/www/medical.seorangeksa.com/*
+# cp -R build/* /var/www/medical.seorangeksa.com
